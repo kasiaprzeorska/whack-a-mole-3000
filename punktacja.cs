4a572458544d9333be8b071bfc22ae44e2c11f68
bakaneko89 <katarzyna.przeorska@gmail.com>
2017-12-19 20:46:20 +0100

    add points counting script

---
+++ punktacja.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class punktacja : MonoBehaviour {
+	public static int punkty;
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		punkty = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Punkty: " + punkty;
+	}
+}
