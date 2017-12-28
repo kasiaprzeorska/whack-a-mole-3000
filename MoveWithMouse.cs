278138efdf3f56a05e3a9086eaa0a57dfa3a7a2f
Tomasz Jeż <tomasz.jez@jamfsoftware.com>
2017-12-28 19:55:00 +0100

    masz pliki

---
+++ MoveWithMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithMouse : MonoBehaviour {
+
+	public Vector3 targetPos;
+	public Vector3 farawayPoint;
+	Plane plane;
+	
+	void Start () {
+		targetPos = transform.position;
+		plane = new Plane(Vector3.up,transform.position);
+		farawayPoint = new Vector3 (targetPos.x, targetPos.y, targetPos.z + 10);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		float point = 0f;
+
+		if(plane.Raycast(ray, out point))
+			targetPos = ray.GetPoint(point);
+		
+		transform.position = new Vector3(targetPos.x, targetPos.y, targetPos.z);
+		transform.LookAt (farawayPoint);
+	
+	}
+}
---
+++ swing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swing : MonoBehaviour {
+
+    public float smooth = 1F;
+	public bool macha = false;
+	Quaternion startRotation;
+	Quaternion newRotation;
+
+	void Start() {
+		startRotation = gameObject.transform.localRotation;
+	}
+
+    void Update() {
+		if (Input.GetKeyDown(KeyCode.Mouse0) && !macha) {
+			macha = true;
+		}
+		if (macha) {
+			doSwing();
+		}
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+			Debug.Log(gameObject.transform.localRotation.x);
+		}
+    }
+
+	void doSwing()
+	{   
+
+		transform.localRotation = Quaternion.Slerp(transform.rotation, new Quaternion(0f, startRotation.y, startRotation.z, 1f), 0.5f);
+
+		if (gameObject.transform.rotation.x > -0.01 && 
+			gameObject.transform.rotation.x < 0.01) {
+			stopDoing();
+		}
+	}
+
+	void stopDoing() {
+		gameObject.transform.localRotation = startRotation;
+		macha = false;
+	}
+}
